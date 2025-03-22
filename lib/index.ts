76518b664df1c0477f55c47497aaf6591d589cea
Madhava Yallanki <madhava@yallanki.dev>
2025-03-21 17:36:58 -0700

    1.0.7

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export { DynamoMapper } from './mapper/index.js';
 export { Operator } from './mapper/utils.js';
+export type { Filter } from './mapper/utils.js';
 export { EntityBase, entity } from './entities/base.js';
 export type { EntityConfig, ConstructorItem, PrimaryKey } from './entities/base.js';
 export { VersionMismatchError, NotFoundError } from './errors/index.js';
